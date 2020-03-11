b7ed4ecfa530a25873b31a5cd5efe67eafe25193
Brant2001 <brant.pippin@gmail.com>
2020-03-11 15:56:19 -0500

    filtered criminals

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 import { getCriminals } from './criminals/CriminalProvider.js'
-import CriminalList from './criminals/CriminalList.js'
+import { CriminalList } from './criminals/CriminalList.js'
 import { getConvictions } from './convictions/ConvictionProvider.js'
 import ConvictionSelect from './convictions/ConvictionSelect.js'
+
+getConvictions().then(
+   () => ConvictionSelect()
+)
 
 getCriminals().then(
    () => CriminalList()
 )
 
-getConvictions().then(
-   () => ConvictionSelect()
-)
+
